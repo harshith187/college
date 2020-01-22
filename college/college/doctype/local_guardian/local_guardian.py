ee48a0f7bc1d8b912d6aeaf8367a730c8c03ca42
Harshith <harshithraj187@gmail.com>
2020-01-22 16:11:01 +0530

    Revert "resolved student data replication problem"
    This reverts commit 75ef838e324a2605f40ce3b886c712e3f37319e9.

--- college/college/doctype/local_guardian/local_guardian.py
+++ college/college/doctype/local_guardian/local_guardian.py
@@ -9,30 +9,20 @@ from frappe.model.document import Document
 class LocalGuardian(Document):
 	def validate(self):
 		self.name1 = get_faculty_name(self.faculty_id)
+
+
+	def update_student_list(self,start_usn,end_usn):
+		students = frappe.db.sql("""select name from `tabStudent` where name between'{0}' and '{1}'""".format(start_usn,end_usn),as_dict=1)
+		for student in students:
+			student_details = frappe.get_doc("Student",student.name)
+			new_student_entry = frappe.new_doc("Basic Student Information")
+			new_student_entry.usn = student_details.usn
+			new_student_entry.name1 = " ".join(filter(None,[student_details.first_name,student_details.middle_name,student_details.last_name]))
+			new_student_entry.email_id = student_details.student_email_id
+			new_student_entry.mobile_number = student_details.student_mobile_number
+			self.append("student_list",new_student_entry)
 			
-	def update_student_list(self,start_usn,end_usn,old_data):
-		new_students = frappe.db.sql("""select name from `tabStudent` where usn between "{0}" and "{1}" """.format(start_usn,end_usn),as_dict=1)
-		usn_list = []
-		if(old_data):
-			for row in old_data:
-				usn_list.append(row['usn'])
-			for student in new_students:
-				student_details = frappe.get_doc("Student",student.name)
-				if student_details.usn not in usn_list:
-					self.append("student_list",update_student_data(student_details))
-		else:
-			for student in new_students:
-				student_details = frappe.get_doc("Student",student.name)
-				self.append("student_list",update_student_data(student_details))
-
-def update_student_data(student_details):
-	new_student_entry = frappe.new_doc("Basic Student Information")
-	new_student_entry.usn = student_details.usn
-	new_student_entry.name1 = " ".join(filter(None,[student_details.first_name,student_details.middle_name,student_details.last_name]))
-	new_student_entry.email_id = student_details.student_email_id
-	new_student_entry.mobile_number = student_details.student_mobile_number
-	return new_student_entry
-
+		
 @frappe.whitelist()
 def get_faculty_name(faculty_id):
 	faculty = frappe.get_doc("Faculty",faculty_id)
